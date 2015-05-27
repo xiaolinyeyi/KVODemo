e5d1db8185799c3bcb54ae0a9fe8574f91e3a719
admin <bupt_z@admindeMacBook-Pro.local>
2015-05-27 17:08:47 +0800

    Initial Commit

---
+++ Observer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Observer
+//
+//  Created by admin on 15/5/27.
+//  Copyright (c) 2015年 admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
